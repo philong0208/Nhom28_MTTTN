70640fb8bfe2ccfa052c2329752fe2c90726996b
MSI\nhatm <nhatminhptithcm@gmail.com>
2023-10-06 03:35:16 +0700

    NN: Entity + DTO + Converter + Repo

---
+++ Source/src/main/java/com/laptrinhjavaweb/repository/TagRepository.java
@@ -0,0 +1,13 @@
+package com.laptrinhjavaweb.repository;
+
+import com.laptrinhjavaweb.entity.TagEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TagRepository extends JpaRepository<TagEntity, Long> {
+    TagEntity findOneByCode(String code);
+    Page<TagEntity> findAll(Pageable pageable);
+    Page<TagEntity> findByNameContainingIgnoreCase(String name, Pageable pageable);
+    long countByNameContainingIgnoreCase(String name);
+}
