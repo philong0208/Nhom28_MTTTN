c170b2e1d63c23a26ab1f014d72314b5ab73c2f0
MSI\nhatm <nhatminhptithcm@gmail.com>
2023-10-06 14:22:15 +0700

    CRUD tag

--- Source/src/main/java/com/laptrinhjavaweb/dto/TagDTO.java
+++ Source/src/main/java/com/laptrinhjavaweb/dto/TagDTO.java
@@ -3,6 +3,9 @@ package com.laptrinhjavaweb.dto;
 public class TagDTO extends AbstractDTO<TagDTO> {
     private String name;
     private String code;
+    private String title;
+    private String content;
+    private String description;
 
     public String getName() {
         return name;
@@ -18,5 +21,29 @@ public class TagDTO extends AbstractDTO<TagDTO> {
 
     public void setCode(String code) {
         this.code = code;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
