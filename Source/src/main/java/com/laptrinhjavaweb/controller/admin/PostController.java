05d06eeed1e4b9de7d27b93013028d2287253da1
MSI\nhatm <nhatminhptithcm@gmail.com>
2023-10-18 12:34:27 +0700

    Push file csv

--- Source/src/main/java/com/laptrinhjavaweb/controller/admin/PostController.java
+++ Source/src/main/java/com/laptrinhjavaweb/controller/admin/PostController.java
@@ -7,7 +7,6 @@ import com.laptrinhjavaweb.service.IAuthorService;
 import com.laptrinhjavaweb.service.ICategoryService;
 import com.laptrinhjavaweb.service.IPostService;
 import com.laptrinhjavaweb.service.ITagService;
-import com.laptrinhjavaweb.service.impl.CsvService;
 import com.laptrinhjavaweb.utils.DisplayTagUtils;
 import com.laptrinhjavaweb.utils.MessageResponseUtils;
 import org.apache.commons.lang.StringUtils;
@@ -69,8 +68,6 @@ public class PostController {
 		mav.addObject(SystemConstant.MODEL, model);
 		return mav;
 	}
-	@Autowired
-	private CsvService csvService;
 
 	private void initMessageResponse(ModelAndView mav, HttpServletRequest request) {
 		String message = request.getParameter("message");
@@ -79,10 +76,5 @@ public class PostController {
 			mav.addObject(SystemConstant.ALERT, messageMap.get(SystemConstant.ALERT));
 			mav.addObject(SystemConstant.MESSAGE_RESPONSE, messageMap.get(SystemConstant.MESSAGE_RESPONSE));
 		}
-		Map<String, String> stringStringMap = csvService.readCsvFile("D:\\Nhatminhptithcm\\Repository\\Workspace\\Nhom28_MTTTN\\Source\\src\\main\\resources\\message\\MessageData.csv");
-		for (Map.Entry<String, String> entry : stringStringMap.entrySet()) {
-			System.out.println(entry.getKey() + " " + entry.getValue());
-		}
-
 	}
 }
--- Source/src/main/java/com/laptrinhjavaweb/utils/MessageResponseUtils.java
+++ Source/src/main/java/com/laptrinhjavaweb/utils/MessageResponseUtils.java
@@ -1,39 +1,24 @@
 package com.laptrinhjavaweb.utils;
 
 import com.laptrinhjavaweb.constant.SystemConstant;
+import com.laptrinhjavaweb.service.impl.CsvService;
+import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.Map;
-
+@Component
 public class MessageResponseUtils {
 
     public static Map<String, String> getMessage(String message) {
+        CsvService csvService = new CsvService();
         Map<String, String> results = new HashMap<>();
-        if (message.equals(SystemConstant.INSERT_SUCCESS)) {
+        Map<String, String> stringStringMap = csvService.readCsvFile("D:\\Nhatminhptithcm\\Repository\\Workspace\\Nhom28_MTTTN\\Source\\src\\main\\resources\\message\\MessageData.csv");
+        if (message.contains("success")) {
             results.put(SystemConstant.ALERT, "success");
-            results.put(SystemConstant.MESSAGE_RESPONSE, "Thêm dữ liệu thành công");
-        } else if (message.equals(SystemConstant.UPDATE_SUCCESS)) {
-            results.put(SystemConstant.ALERT, "success");
-            results.put(SystemConstant.MESSAGE_RESPONSE, "Cập nhật dữ liệu thành công");
-        } else if (message.equals(SystemConstant.DELETE_SUCCESS)) {
-            results.put(SystemConstant.ALERT, "success");
-            results.put(SystemConstant.MESSAGE_RESPONSE, "Xóa dữ liệu thành công");
-        } else if (message.equals(SystemConstant.ERROR_SYSTEM)) {
+        } else {
             results.put(SystemConstant.ALERT, "danger");
-            results.put(SystemConstant.MESSAGE_RESPONSE, "Đã có lỗi xảy ra, vui lòng thử lại sau");
-        } else if (message.equals(SystemConstant.INSERT_FAILED)) {
-            results.put(SystemConstant.ALERT, "danger");
-            results.put(SystemConstant.MESSAGE_RESPONSE, "Dữ liệu này đã tồn tại, vui lòng nhập lại");
-        } else if (message.equals(SystemConstant.UPDATE_FAILED)) {
-            results.put(SystemConstant.ALERT, "danger");
-            results.put(SystemConstant.MESSAGE_RESPONSE, "Dữ liệu này đã tồn tại, cập nhật thất bại");
-        } else if (message.equals(SystemConstant.DELETE_FAILED)) {
-            results.put(SystemConstant.ALERT, "danger");
-            results.put(SystemConstant.MESSAGE_RESPONSE, "Xóa thất bại, đảm bảo rằng dữ liệu bạn xóa không được sử dụng ở bất kỳ đâu");
-        } else if (message.equals(SystemConstant.DATA_ACCESS_DENIED)) {
-            results.put(SystemConstant.ALERT, "danger");
-            results.put(SystemConstant.MESSAGE_RESPONSE, "Bạn không có quyền truy cập vào dữ liệu này");
         }
+        results.put(SystemConstant.MESSAGE_RESPONSE, stringStringMap.get(message));
         return results;
     }
 }
